654baf6894a2e7e1830874426ad38e9ef06e5f36
mfiim <fajar12345678905@gmail.com>
2023-06-04 21:35:45 +0700

    mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -25,10 +25,8 @@ with st.sidebar:
     
     selected = option_menu('Multiple Disease Prediction System',
                           
-                          ['Diabetes Prediction',
-                           'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
+                          ['Diabetes Prediction'],
+                          icons=['activity'],
                           default_index=0)
     
     
